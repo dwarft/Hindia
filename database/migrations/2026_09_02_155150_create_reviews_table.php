590742b77040b8b93fed4ae93c7a673cb88245fa
cr7zone <koksonmokson@gmail.com>
2026-09-02 23:54:40 +0800

    buat migrations review

--- database/migrations/2026_09_02_155150_create_reviews_table.php
+++ database/migrations/2026_09_02_155150_create_reviews_table.php
@@ -10,12 +10,19 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
-        Schema::create('reviews', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+{
+    Schema::create('reviews', function (Blueprint $table) {
+        $table->id();
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->unsignedBigInteger('movie_id'); // ID film dari TMDB
+        $table->tinyInteger('rating')->comment('Nilai 1 - 5');
+        $table->text('comment');
+        $table->timestamps();
+
+        // Mencegah user mengulas film yang sama berkali-kali
+        $table->unique(['user_id', 'movie_id']);
+    });
+}
 
     /**
      * Reverse the migrations.
